f15d50e87c743063cc1a9c9d16726166704ed5ae
Sara Dzemidzic <sara.dzemidzic@edu.fit.ba>
2022-02-23 20:52:52 +0100

    Add initial services and Http helpers

---
+++ Rentacar.Admin/Rentacar.Admin.Services/HttpHelper.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Rentacar.Admin.Services
+{
+    public static class HttpHelper
+    {
+        public static string BuildQueryParams(string baseUrl, Dictionary<string, string> parameters)
+        {
+            var builder = new UriBuilder(baseUrl)
+            {
+                Port = -1
+            };
+            var query = HttpUtility.ParseQueryString(builder.Query);
+
+            foreach (var parameter in parameters)
+            {
+                query[parameter.Key] = parameter.Value;
+            }
+
+            builder.Query = query.ToString();
+
+            return builder.ToString();
+        }
+    }
+}
---
+++ Rentacar.Admin/Rentacar.Admin.Services/UserService.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using Rentacar.Dto;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Rentacar.Admin.Services
+{
+    public static class UserService
+    {
+        private static string _baseUrl = System.Configuration.ConfigurationManager.AppSettings["baseUrl"] + "/api/users";
+
+        private static HttpClient _httpClient = new HttpClient();
+
+
+        public static async Task<List<UserDto>> FilterUsers(string userId, string firstName, string lastName, string email)
+        {
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            parameters["userId"] = userId;
+            parameters["firstName"] = firstName;
+            parameters["lastName"] = lastName;
+            parameters["email"] = email;
+
+            var filterUsersUrl = HttpHelper.BuildQueryParams(_baseUrl + "/filter", parameters);
+
+            HttpResponseMessage response = await _httpClient.GetAsync(filterUsersUrl);
+            var jsonObject = await response.Content.ReadAsStringAsync();
+            var listOfUsers = JsonConvert.DeserializeObject<List<UserDto>>(jsonObject);
+
+            return listOfUsers;
+        }
+
+        public static async Task DeleteUser(string userId)
+        {
+            await _httpClient.DeleteAsync($"{_baseUrl}/{userId}");
+        }
+    }
+}
