68ce7908cbe75c472e0fea54342c3705627b96ae
Sara Dzemidzic <Sara.Dzemidzic@iwgplc.com>
2022-06-12 22:15:06 +0200

    Booking

--- rentacar.flutter/lib/models/booking.dart
+++ rentacar.flutter/lib/models/booking.dart
@@ -10,4 +10,41 @@ class Booking {
   int vehicleId = 0;
   double? totalPrice = 0;
   CardInfo? cardInfo;
+
+  Booking(
+      this.bookingId,
+      this.startDate,
+      this.endDate,
+      this.createdDate,
+      this.updatedDate,
+      this.userId,
+      this.vehicleId,
+      this.totalPrice,
+      this.cardInfo);
+
+  Booking.fromJson(Map<String, dynamic> bookingMap) {
+    bookingId = bookingMap['bookingId'];
+    startDate = DateTime.parse(bookingMap['startDate']);
+    endDate = DateTime.parse(bookingMap['endDate']);
+    createdDate = DateTime.parse(bookingMap['createdDate']);
+    updatedDate = DateTime.parse(bookingMap['updatedDate']);
+    userId = bookingMap['userId'];
+    vehicleId = bookingMap['vehicleId'];
+    totalPrice = bookingMap['totalPrice'];
+    cardInfo = bookingMap['cardInfo'];
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'bookingId': bookingId,
+      'startDate': startDate?.toIso8601String(),
+      'endDate': endDate?.toIso8601String(),
+      'createdDate': createdDate?.toIso8601String(),
+      'updatedDate': updatedDate?.toIso8601String(),
+      'userId': userId,
+      'vehicleId': vehicleId,
+      'totalPrice': totalPrice,
+      'cardInfo': cardInfo
+    };
+  }
 }
